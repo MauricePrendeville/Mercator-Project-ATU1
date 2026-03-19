cd82f38dfc29ef451af52239e92994a6ac561908
Maurice Prendeville <237830547+MauricePrendeville@users.noreply.github.com>
2026-03-19 22:16:05 +0000

    Initial Model Classes. Task, User, VendorCandidate

---
+++ VendorProcessManagerV1/Models/Task.cs
@@ -0,0 +1,24 @@
+﻿namespace VendorProcessManagerV1.Models
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public Guid TaskId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+        public Guid Creator {  get; set; }
+        public Guid Owner { get; set; }
+        public string? ApproverTeam { get; set; }
+        public Guid? Approver {  get; set; }
+        public DateTime? ApproveDate { get; set; }
+        public string? ApproveStatus { get; set; } //change to Enum
+        public List<Task>? DependsOn { get; set; }
+        public List<Task>? SuccessorTasks { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool RequiresApproval { get; set; }
+
+    }
+}
---
+++ VendorProcessManagerV1/Models/VendorCandidate.cs
@@ -0,0 +1,12 @@
+﻿namespace VendorProcessManagerV1.Models
+{
+    public class VendorCandidate
+    { 
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; } = DateTime.MinValue;
+        public Guid Creator {  get; set; }
+
+    }
+}
