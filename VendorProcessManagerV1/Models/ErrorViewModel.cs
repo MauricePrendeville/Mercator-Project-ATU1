2346637c726b79e8a74637538720e109726c3199
Maurice Prendeville <237830547+MauricePrendeville@users.noreply.github.com>
2026-03-19 21:56:28 +0000

    Add project files.

---
+++ VendorProcessManagerV1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendorProcessManagerV1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
