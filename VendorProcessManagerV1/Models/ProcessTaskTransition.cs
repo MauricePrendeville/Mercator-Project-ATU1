b5f206eb0d318c8aa5fe4141a7ce9e38a97a1124
Maurice Prendeville <237830547+MauricePrendeville@users.noreply.github.com>
2026-04-14 23:58:28 +0100

    Alternate Task transition Models. Should make multi-path flow of tasks easier to create. Also updates to Layout file to add Bootstrap themes

---
+++ VendorProcessManagerV1/Models/ProcessTaskTransition.cs
@@ -0,0 +1,13 @@
+﻿namespace VendorProcessManagerV1.Models
+{
+    public class ProcessTaskTransition
+    {
+        public Guid Id { get; set; }
+        public Guid ProcessInstanceId { get; set; }
+        public Guid FromProcessStepId { get; set; }
+        public Guid ToProcessStepId { get; set; }
+        public Guid ActionUserId { get; set; }
+        public DateTime ActionDate { get; set; }
+        public string ActionValue { get; set; }
+    }
+}
---
+++ VendorProcessManagerV1/Models/ProcessTemplateTransition.cs
@@ -0,0 +1,16 @@
+﻿namespace VendorProcessManagerV1.Models
+{
+    public class ProcessTemplateTransition
+    {
+        public Guid Id { get; set; }
+        public Guid ProcessTemplateId { get; set; }
+        public Guid FromProcessTemplateTaskId { get; set; }
+        public Guid ToProcessTemplateTaskId { get; set; }
+        public string DisplayLabel { get; set; }
+        public int SortOrder { get; set; }
+        //public string? ConditionType { get; set; }
+        //public int? ConditionValue  { get; set; }
+        //public bool? IsDefault { get; set; }
+
+    }
+}
